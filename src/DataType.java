89eeff934ba3aa8678ba7e4bb401836b17a8a84a
Shah-jalal <shah7097tn@gmail.com>
2018-01-24 13:40:50 -0600

    first commit for Java basic

---
+++ src/DataType.java
@@ -0,0 +1,46 @@
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO: Java - Basic Data Type 
+		//Primitive Data Types -->> There are eight primitive data types in Java
+		
+		//byte: Byte data type is an 8-bit signed two's complement integer
+		byte b1 = 100;
+		byte b2 = -50;
+		
+		//short: Short data type is a 16-bit signed two's complement integer
+		short s1 = 10000;
+		short s2 = -20000;
+		
+		//int: Int data type is a 32-bit signed two's complement integer
+		int i1 = 100000;
+		int i2 = -200000;
+		
+		//long: Long data type is a 64-bit signed two's complement integer
+		long l1 = 100000L;
+		long l2 = -200000L;
+		
+		//float: Float data type is a single-precision 32-bit IEEE 754 floating point
+		float f1 = 234.5f;
+		
+		//double: Double data type is a double-precision 64-bit IEEE 754 floating point
+		double  sineVal = 12345.234d;
+		
+		//boolean: boolean data type represents one bit of information
+		//There are only two possible values: true and false
+		//Default value is false
+		boolean flag = true;
+		
+		//char: char data type is a single 16-bit Unicode character
+		char letterA = 'A';
+				
+		
+				
+				
+		
+		
+
+	}
+
+}
